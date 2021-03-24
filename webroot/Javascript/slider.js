b4e44fd066b0d4c1729dc7091422c6a3f33d2163
Peta Jeannie <peta.jea@onja.org>
2021-03-24 14:03:31 -0600

    having changed some marging and width

--- webroot/Javascript/slider.js
+++ webroot/Javascript/slider.js
@@ -1,29 +1,33 @@
 function Slider(slider) {
-    if (!slider) {
-      throw Error('No Gallerry Found!!!');
+  const images = Array.from(slider.querySelectorAll("img"));
+  const bigImageDisplay = document.querySelector(".ChoosenImg");
+
+  if (!slider) {
+    throw Error("No Gallerry Found!!!");
   }
-  const images = Array.from(slider.querySelectorAll('img'));
-  const bigImageDisplay = document.querySelector('.ChoosenImg');
-  
+
   function showImage(el) {
     if (!el) {
-      console.info('no image to show');
+      console.info("no image to show");
       return;
-    };
-    bigImageDisplay.querySelector('img').src = el.src;
+    }
+    bigImageDisplay.querySelector("img").src = el.src;
 
-    images.forEach(image => {
-
-      if(image.classList.contains("selected-thumbnil")) {
+    images.forEach((image) => {
+      image.classList.add("selected-thumbnil");
+      if (image.classList.contains("selected-thumbnil")) {
         image.classList.remove("selected-thumbnil");
+        images[0].style.opacity = "0.5";
       }
     });
     el.classList.add("selected-thumbnil");
-    console.log("source",  el.src);
-    }
+    console.log("source", el.src);
+  }
 
-  images.forEach(image => {
-    image.addEventListener('click', e => showImage(e.currentTarget));
+  images[0].style.opacity = "1";
+
+  images.forEach((image) => {
+    image.addEventListener("click", (e) => showImage(e.currentTarget));
   });
 }
-const mySlider = Slider(document.querySelector('.sliders'));
+const mySlider = Slider(document.querySelector(".sliders"));
